7d6afa723edbf38c2394256ccf4d299fd234d0c4
Henrique Freitas <freitas.henrique1999@gmail.com>
2019-11-05 13:15:06 +0000

    beginning of projec

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+    include_once('templates/head.php');
+    echo "<h1>Hello world</h1>";
+?>
---
+++ templates/head.php
@@ -0,0 +1,26 @@
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Moties</title>
+</head>
+
+<body>
+    <header>
+        <a href=""></a>
+    <nav>
+        <ul>
+            <li><a href="">Host a Home</a></li>
+            <li><a href="">Log in</a></li>
+            <li><a href="">Sign up</a></li>
+            <li><a href="">Help</a></li>
+        </ul>
+    </nav>
+    </header>
+    
+</body>
+
+</html>
