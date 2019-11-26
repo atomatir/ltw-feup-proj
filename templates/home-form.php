bae6229ffad8c56f737832a3f3fb23c91c7895f6
Henrique Freitas <freitas.henrique1999@gmail.com>
2019-11-26 13:10:43 +0000

    work done in class, html and css

--- templates/home-form.php
+++ templates/home-form.php
@@ -1,24 +1,25 @@
-<div id="outer-form">
-            <form id="search-form" action="">
-                <label for="city">
-                    <input type="text" name="" id="city" />
-                </label>
 
-                <label for="dates">
-                    <input type="date" name="" id="date-in" />
-                    <input type="date" name="" id="date-out" />
-                </label>
+    <div id="outer-form">
+        <form id="search-form" action="">
+            <label for="city">
+                <input type="text" name="" id="city" placeholder="Where do you wanna go?" />
+            </label>
 
-                <label for="n-guests">
-                    <input type="text" name="" id="n-guests" />
-                </label>
+            <label for="dates">
+                <input type="date" name="" id="date-in" placeholder="" />
+                <input type="date" name="" id="date-out" />
+            </label>
 
-                <label for="price-range">
-                    <input type="range" name="" id="price" min="0" max="2000" />
-                </label>
+            <label for="n-guests">
+                <input type="text" name="" id="n-guests" placeholder="What is the number of guests?" />
+            </label>
 
-                <label for="search">
-                    <input type="submit" value="Search" />
-                </label>
-            </form>
-        </div>
+            <label for="price-range">
+                <input type="range" name="" id="price" min="0" max="2000" />
+            </label>
+
+            <label id="search-button" for="search">
+                <input id="search-button-input" type="submit" value="Search" />
+            </label>
+        </form>
+    </div>
