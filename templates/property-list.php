623b300d93af4449e2705ffba2c877f11c20d7c1
henrique <up201707046@fe.up.pt>
2019-12-03 13:13:54 +0000

    started property list element

---
+++ templates/property-list.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/property-list.css">
+    <link rel="stylesheet" href="../css/head.css">
+    <title>Document</title>
+</head>
+<body>
+<section class="property">
+    <img class="property-pic" src="../images/background-house.jpg" alt="house picture">
+    <div class="property-text">
+        <div class="property-info">
+            <h2 class="property-typo">Private Room</h2>
+            <img src="../images/Full House.png" alt="" >
+            <h2 class="property-rating">(1,5)</h2>
+            <div class="property-specs"></div>
+        </div>
+        <h1 class="property-name">Clérigos Room</h1>
+        <div class="property-specs"></div>
+    </div>
+</section>
+
+
+</body>
+</html>
+
