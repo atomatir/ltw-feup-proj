2e26f646c8c77a90ba3ebde76fef3a4b6a1a0ba9
Filipe Ferreira <filipejosexavierferreira@gmail.com>
2019-12-07 15:31:57 +0000

    Fixed include, started placeform

---
+++ templates/place-form.php
@@ -0,0 +1,18 @@
+<form action="" method="post">
+  <input type="text" name="title" id="place-title">
+  <textarea name="desc" id="place-desc" cols="10" rows="10"></textarea>
+  <input type="number" name="bathrooms" id="place-bathrooms">
+  <input type="number" name="bedrooms" id="place-bedrooms">
+  <input type="number" name="max_guests" id="place-guests">
+  <input type="number" name="price_by_nigth" id="place-price">
+
+  <input type="number" name="latitude" id="place-lat">
+  <input type="number" name="longitude" id="place-long">
+  <input type="number" name="postal_code" id="place-long">
+  <input type="text" name="address" id="place-address">
+  <input type="number" name="street_number" id="place-street">
+
+  
+
+
+</form>
