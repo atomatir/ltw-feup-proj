6c6477fd75b3a7bd67f5c8b7ca145a1a065d6dab
Rita Nunes da Mota <up201703964@gnomo.fe.up.pt>
2019-12-07 20:06:16 +0000

    css new place page kinda finished

--- templates/place-form.php
+++ templates/place-form.php
@@ -7,14 +7,14 @@
     <input type="number" name="bathrooms" id="place-bathrooms" min=0 class="input" placeholder="# Bathrooms">
     <input type="number" name="bedrooms" id="place-bedrooms" min=0 class="input" placeholder="# Bedrooms">
     <input type="number" name="max_guests" id="place-guests" min=1 class="input" placeholder="Max # of guests">
-    <input type="number" name="price_by_nigth" id="place-price" min=0 class="input" placeholder="Price per night">
+    <input type="number" name="price_by_nigth" id="place-price" min=0 class="input" placeholder="Price p/Night">
   </div>
   <div id='address-div'>
-    <input type="number" name="latitude" id="place-lat" class="input" placeholder="Price per night">
-    <input type="number" name="longitude" id="place-long" class="input">
+    <input type="number" name="latitude" id="place-lat" class="input" placeholder="Latitude(?)">
+    <input type="number" name="longitude" id="place-long" class="input"placeholder="Longitude(?)">
     <input id="street-name" type="text" name="address" id="place-address" class="input" placeholder="Street name">
     <input id="street-no" type="number" name="street_number" id="place-street" class="input" placeholder="No.">
-    <input type="number" name="postal_code" id="place-long" class="input" >
+    <input type="number" name="postal_code" id="place-long" class="input" placeholder="Postal Code">
 
   </div>
 
