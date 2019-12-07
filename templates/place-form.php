2e96116f7e1d8f50f3bc410fded2dcc8d9af5407
Rita Nunes da Mota <up201703964@gnomo.fe.up.pt>
2019-12-07 20:50:58 +0000

    LAST TIME?

--- templates/place-form.php
+++ templates/place-form.php
@@ -2,19 +2,19 @@
 <h2>CREATING A NEW PLACE</h2>
 <form id="place-form" action="" method="post">
   <div id="place-div">
-    <input type="text" name="title" id="place-title" class="input" placeholder="Name">
-    <textarea name="desc" id="place-desc" maxlength="450" cols="10" rows="10" class="input" placeholder="Description"></textarea>
-    <input type="number" name="bathrooms" id="place-bathrooms" min=0 class="input" placeholder="# Bathrooms">
-    <input type="number" name="bedrooms" id="place-bedrooms" min=0 class="input" placeholder="# Bedrooms">
-    <input type="number" name="max_guests" id="place-guests" min=1 class="input" placeholder="Max # of guests">
-    <input type="number" name="price_by_nigth" id="place-price" min=0 class="input" placeholder="Price p/Night">
+    <input    id="place-name"   type="text"   name="title"                class="input" placeholder="Name">
+    <textarea id="place-desc" name="desc" maxlength="450" cols="10" rows="10" class="input" placeholder="Description"></textarea>
+    <input    id="place-beds"   type="number" name="bedrooms"       min=0 class="input" placeholder="# Bedrooms">
+    <input    id="place-baths"  type="number" name="bathrooms"      min=0 class="input" placeholder="# Bathrooms">
+    <input    id="place-guests" type="number" name="max_guests"     min=1 class="input" placeholder="Max # of guests">
+    <input    id="place-price"  type="number" name="price_by_nigth" min=0 class="input" placeholder="$/Night">
   </div>
   <div id='address-div'>
-    <input type="number" name="latitude" id="place-lat" class="input" placeholder="Latitude(?)">
-    <input type="number" name="longitude" id="place-long" class="input"placeholder="Longitude(?)">
-    <input id="street-name" type="text" name="address" id="place-address" class="input" placeholder="Street name">
-    <input id="street-no" type="number" name="street_number" id="place-street" class="input" placeholder="No.">
-    <input type="number" name="postal_code" id="place-long" class="input" placeholder="Postal Code">
+    <input id="place-lat"   type="number" name="latitude"      class="input" placeholder="Latitude(?)">
+    <input id="place-long"  type="number" name="longitude"     class="input" placeholder="Longitude(?)">
+    <input id="place-street" type="text"   name="address"       class="input" placeholder="Street name">
+    <input id="place-num"   type="number" name="street_number" class="input" placeholder="No.">
+    <input id="place-pcode"  type="text" name="postal_code"   class="input" placeholder="Postal Code">
 
   </div>
 
