ebfbfe7d5e6db09234d655ffac7757bff433cf1c
Rita Nunes da Mota <up201703964@gnomo.fe.up.pt>
2019-12-09 01:38:20 +0000

    O DROPDOWN ESTÁ CENTRADO
    agora so falta perceber o problema que criei ao conseguir centrar :')ahahmatemme:')

--- templates/login-dropdown.php
+++ templates/login-dropdown.php
@@ -3,8 +3,9 @@
     <img id="profile_img" src="../images/profile.png" alt="profile picture">
     <p><?php echo $_SESSION['firstName'] . " " . $_SESSION['lastName']; ?></p>
   </a>
-  <ul class="dropdown">
-<li><a href="#" class="nav_button">Edit Profile</a></li>
-<li><a href="../database/action_logout.php" class="nav_button">Log Out</a></li>
-</ul>
+
+  <div class="dropdown">
+    <a href="#" class="nav_button">Edit Profile</a>
+   <a href="../database/action_logout.php" class="nav_button">Log Out</a>
+</div>
 </li>
