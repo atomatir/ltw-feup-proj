0a04e1a0c0ca355307e0f6d5e5fb3c6ee5635f20
henrique <up201707046@fe.up.pt>
2019-12-13 17:10:53 +0000

    home form stuff

---
+++ database/action_host.php
@@ -0,0 +1,16 @@
+<?php
+
+    function getRegionId() {}
+
+    function getCountryId() {}
+
+    function getStateId() {}
+
+    function getCityId() {}
+
+    function getAddressId() {}
+
+    
+
+    header("Location: ../pages/profile-page.php");
+?>
